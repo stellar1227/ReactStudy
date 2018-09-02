e1a1f3acdd7dfdfadcf1d6a82714ee632dc977a7
stellar1227 <halfjin7@gmail.com>
2018-09-02 17:30:56 +0900

    JSX 작성 및 이해

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component , Fragment } from 'react';
 
 class App extends Component {
   render() {
+    const text = '잘 해보자!'; {/*const 상수선언시, let 변수선언시*/} 
+    const condition = true;
+    const style = {
+      backgroundColor : 'gray',
+      fontSize : '30px',
+      width : Math.round(Math.random() * 300) + 50
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>        
+        {/* 하나로 감싸는 부모요소가 있어야 함 , 불필요한 div 사용대신 fragment 객체를 사용 */}
+        <h1 className="my_div">리액트 안녕!</h1>  
+        <p>{text}</p>
+        {
+          condition ? '참' : '거짓' 
+        }
+        {
+          condition && '보여주세요'
+        }
+        <div style={style}>어때?</div>
+      </Fragment>
     );
   }
 }
